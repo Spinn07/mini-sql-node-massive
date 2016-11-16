7bd83763e5a9826b4f9856fb6cfda1efdc8c6352
Henry Reed <henryreed@Henrys-MacBook-Pro.local>
2016-11-16 16:30:44 -0700

    finished project

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var massive = require('massive');
+
 
 var app = module.exports = express();
+
+
+var connectionString = "postgres://henryreed@localhost/sandbox";
+var massiveInstance = massive.connectSync({connectionString : connectionString});
+app.set('db', massiveInstance);
+var db = app.get('db');
+var controller = require('./controller.js');
+
+controller.getPlanes();
+
+// db.new_plane(function(err, planes){
+//   console.log(err, "plane added");
+// });
+// db.get_planes(function(err, planes){
+//   console.log(err, planes);
+// });
+
+
+
+
+
+
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen('3000', function(){
-  console.log("Successfully listening on : 3000")	
-})
 
+app.listen('3000', function(){
+  console.log("Successfully listening on : 3000");
+});
---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE airplanes (
+  planeid SERIAL PRIMARY KEY NOT NULL, -- The primary key
+  planetype varchar(40) NOT NULL, -- The IP of the host
+  passengercount integer NOT NULL -- The name of the host
+
+);
